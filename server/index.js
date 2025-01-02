c359c82dadd9a30baec8c62bfe91179fa587ee62
unknown <oamenemmanuel22@gmail.com>
2025-01-02 11:49:09 +0100

    updating backend payment options

--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/connectDB.js";
-
 import cors from "cors";
-
 import bodyParser from "body-parser";
 import axios from "axios";
 import crypto from "crypto";
+import telegramRoutes from "./routes/telegramRoutes.js";
 
 dotenv.config();
+
 
 const dbUrl = process.env.DB_URL;
 
 connectDB(dbUrl);
 const app = express();
-
+app.use(express.json());
 app.use(bodyParser.json());
-
-
-
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
 
 
-
+app.use(telegramRoutes);
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
