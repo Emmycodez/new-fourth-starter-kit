0dcd26204647374ad4dc0c0fbd114d942c7ff814
unknown <oamenemmanuel22@gmail.com>
2025-01-09 21:14:30 +0100

    removed middleware file

--- client/app/middleware.js
+++
@@ -1,9 +0,0 @@
-import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware"
-
-export default function middleware(request) {
-  return withAuth(request);
-}
-
-export const config = {
-  matcher: ["/site/dashboard"]
-}
